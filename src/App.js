5685eb4b356b33392da0605ab80a8f6d7c4a048f
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-03 14:15:15 +0100

    add router paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import Calculator from './components/Calculator';
 import Quotes from './components/Quotes';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/calucator",
+    element: <Calculator />
+  },
+  {
+    path: "quotes",
+    element: <Quotes />
+  }
+])
 
 export default function App() {
   return (
     <div className="max-w-2xl container mx-auto flex flex-col mt-4 items-center min-h-screen">
-      <Calculator />
-      <Quotes />
+      <div>
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
