23d0c11109d70dd92fae0aa607083e7706f38910
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-03 16:33:38 +0100

    Add Navbar and links to routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quotes from './components/Quotes';
 import Home from './components/Home';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/calculator",
-    element: <Calculator />,
-  },
-  {
-    path: "quotes",
-    element: <Quotes />,
-  },
-]);
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
-    <div className="max-w-2xl container mx-auto flex flex-col mt-4 items-center min-h-screen">
-      <div className="w-full">
-        <RouterProvider router={router} />
-      </div>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="max-w-2xl container mx-auto flex flex-col mt-4 items-center justify-center min-h-[50vh]">
+          <div className="w-full mt-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/calculator" element={<Calculator />} />
+              <Route path="/quotes" element={<Quotes />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
